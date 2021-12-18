5cb8a07f7980e500209e7895893fffa13a6fa097
AngieGarzon22 <angie.garzon.mt@correo.usa.edu.co>
2021-12-18 17:40:34 -0500

    secont commit

--- src/main/java/com/usa/ciclo4/reto2ciclo4/repository/OrderRepository.java
+++ src/main/java/com/usa/ciclo4/reto2ciclo4/repository/OrderRepository.java
@@ -4,9 +4,12 @@ import com.usa.ciclo4.reto2ciclo4.model.Order;
 import com.usa.ciclo4.reto2ciclo4.repository.crud.OrderCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,13 +48,23 @@ public class OrderRepository {
         return orderCrudRepository.findBySalesManIdAndStatus(id, status);
     }
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     public List<Order> getRegisterDayId(String registerDay, Integer id){
-        try{
-            return orderCrudRepository.findByRegisterDayAndSalesManId(new SimpleDateFormat("yyyy-MM-dd").parse(registerDay), id);
-        } catch (ParseException error){
-            System.out.println(error);
-            return null;
-        }
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Query query = new Query();
+
+        Criteria dateCriteria = Criteria.where("registerDay")
+                .gte(LocalDate.parse(registerDay, dtf).minusDays(1).atStartOfDay())
+                .lt(LocalDate.parse(registerDay, dtf).plusDays(1).atStartOfDay())
+                .and("salesMan.id").is(id);
+
+        query.addCriteria(dateCriteria);
+
+        return mongoTemplate.find(query,Order.class);
     }
 
 }
+
+
